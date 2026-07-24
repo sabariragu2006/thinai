5e291c01ce321491deacffc16181e82e178c79ad
Sabari Ragu <sabariragu2006@gmail.com>
2026-07-24 09:08:15 +0530

    committed

---
+++ backend/schema-platform/src/main/java/com/schemaforge/schema_platform/SchemaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.schemaforge.schema_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaPlatformApplication.class, args);
+	}
+
+}
